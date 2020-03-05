05e5cf3c63f838f2fabfe64103bbfb7d7b8a8e8e
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2020-03-05 11:00:11 +0600

    Add project files.

---
+++ PasswordTextTransferFromClientToServer/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PasswordTextTransferFromClientToServer.Models;
+
+namespace PasswordTextTransferFromClientToServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login aLogin)
+        {
+            return View();
+        }
+        public IActionResult Registration(Login aLogin)
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PasswordTextTransferFromClientToServer/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace PasswordTextTransferFromClientToServer.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string LoginPassword { get; set; }
+
+    }
+}
